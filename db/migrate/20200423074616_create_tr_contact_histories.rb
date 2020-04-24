d2422eae4c56d033d36d03d57eb7081862952ba3
hir-ogura <business.yyoo@gmail.com>
2020-04-24 06:08:14 +0000

    add Constraints

--- db/migrate/20200423074616_create_tr_contact_histories.rb
+++ db/migrate/20200423074616_create_tr_contact_histories.rb
@@ -1,8 +1,8 @@
 class CreateTrContactHistories < ActiveRecord::Migration[5.2]
   def change
-    create_table :tr_contact_histories do |t|
-      t.integer :staff_no
-      t.datetime :contact_datetime
+    create_table :tr_contact_histories, primary_key: :unique_id do |t|
+      t.integer :staff_no :limit => 4
+      t.datetime :contact_datetime :null => false
       t.datetime :insert_datetime
       t.datetime :update_datetime
       
