1ee7895d801a7b1775f7038835cfebab7981d798
hir-ogura <business.yyoo@gmail.com>
2020-04-24 08:48:02 +0000

    fix migrate

--- db/migrate/20200423074616_create_tr_contact_histories.rb
+++ db/migrate/20200423074616_create_tr_contact_histories.rb
@@ -1,7 +1,7 @@
 class CreateTrContactHistories < ActiveRecord::Migration[5.2]
   def change
     create_table :tr_contact_histories, primary_key: :unique_id, unsigned: true do |t|
-      t.integer :staff_no, :limit => 4
+      t.string :staff_no, :limit => 4
       t.datetime :contact_datetime, null: false
       t.datetime :insert_datetime
       t.datetime :update_datetime
