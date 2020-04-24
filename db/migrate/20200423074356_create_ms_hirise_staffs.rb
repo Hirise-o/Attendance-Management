95f6dd94b097ec734654b2a05047c9efb07a4776
hir-ogura <business.yyoo@gmail.com>
2020-04-24 07:34:16 +0000

    fix migrate

--- db/migrate/20200423074356_create_ms_hirise_staffs.rb
+++ db/migrate/20200423074356_create_ms_hirise_staffs.rb
@@ -1,8 +1,8 @@
 class CreateMsHiriseStaffs < ActiveRecord::Migration[5.2]
   def change
-    create_table :ms_hirise_staffs do |t|
-      t.integer :staff_no
-      t.string :staff_name
+    create_table :ms_hirise_staffs,  id: false, primary_key: :staff_no do |t|
+      t.string :staff_no, :limit => 4
+      t.string :staff_name, :limit => 72
       t.datetime :insert_datetime
       t.datetime :update_datetime
       
--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_23_074616) do
 
-  create_table "ms_hirise_staffs", force: :cascade do |t|
-    t.integer "staff_no"
-    t.string "staff_name"
+  create_table "ms_hirise_staffs", id: false, force: :cascade do |t|
+    t.string "staff_no", limit: 4
+    t.string "staff_name", limit: 72
     t.datetime "insert_datetime"
     t.datetime "update_datetime"
     t.datetime "created_at", null: false
