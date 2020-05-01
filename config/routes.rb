d1c599ea230c6330ba74e6f5a35d70cdebdfd0fc
hir-ogura <business.yyoo@gmail.com>
2020-05-01 08:28:39 +0000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'tr_contact_history/main'
   get 'top' => 'root#root'
-  root to: "tr_contact_history#main"
   post 'tr_contact_history' => 'tr_contact_history#create'
 end
